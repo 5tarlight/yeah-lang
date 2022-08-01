14f5815f45b9a2eb88b0ecd46cf7d085c09bb5cc
YEAHx4 <developerstarlight@gmail.com>
2022-08-02 01:26:08 +0900

    main class

--- src/main/java/com/yeahx4/Main.java
+++ src/main/java/com/yeahx4/Main.java
@@ -1,7 +1,13 @@
 package com.yeahx4;
 
 public class Main {
+    private static void printHelp() {
+
+    }
+
     public static void main(String[] args) {
+        if (args.length == 0) printHelp();
+
         System.out.println("Hello world!");
     }
 }
